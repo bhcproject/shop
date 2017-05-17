ef43a56e1bbf8ba1a36db7ddea0ba820dbecf371
baohuachang <bhchang.paul@gmail.com>
2017-05-16 22:05:07 -0500

    init

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ var express = require('express'),
 Object.assign=require('object-assign')
 
 app.engine('html', require('ejs').renderFile);
+app.engine('pug', require('pug').renderFile);
+
 app.use(morgan('combined'))
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
@@ -72,10 +74,12 @@ app.get('/', function (req, res) {
     // Create a document with request IP and current time of request
     col.insert({ip: req.ip, date: Date.now()});
     col.count(function(err, count){
-      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails, item: 'item' });
+      res.render('index.pug', {title:"phoebe shop, db connected"});
+      //res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails, item: 'item' });
     });
   } else {
-    res.render('index.html', { pageCountMessage : null});
+    res.render('index.pug', {title:"phoebe shop, db not connected"});
+    //res.render('index.html', { pageCountMessage : null});
   }
 });
 
